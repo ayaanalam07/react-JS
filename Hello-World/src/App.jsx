e25080d9f64f642ec9a06e7c6a0e12d95f5a597c
ayaanalam07 <ayaanabaf@gmail.com>
2024-09-03 09:47:36 +0500

    completed

--- Hello-World/src/App.jsx
+++ Hello-World/src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
+const [num , setNum] = useState(0);
+
+function addBtn(){
+  setNum(num + 1);
+}
+
+function lessBtn(){
+  if(num === 0){
+    alert("You can't go below 0");
+  }else{
+    setNum(num - 1);
+  }
+}
+
+function resetBtn(){
+  if(num === 0){
+    alert("Already Num Value is 0")
+  }else{
+    setNum(0);
+  }
+}
+
   return (
     <>
-      <div>
-      <h1>Hello World</h1>
-      </div>
+  <h1>{num}</h1>
+  <button onClick={addBtn}>Add Num</button>
+  <button onClick={lessBtn}>Less Num</button>
+  <button onClick={resetBtn}>Reset</button>
+
+
     </>
   )
 }
---
+++ Hello-World/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+
+        
+
+  return (
+    <>
+    <h1></h1>
+    <button>click</button>
+    </>
+  )
+}
+
+export default Card
